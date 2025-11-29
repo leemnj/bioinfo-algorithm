4db77755ad1caa3d2d1d7a308e32792333291560
Minjae Lee <dlalswo0321@gmail.com>
2025-11-29 21:46:12 +0900

    Remove old uppercase directories and keep lowercase only

--- Chapter_1/bioinfo_1C.py
+++
@@ -1,9 +0,0 @@
-def ReverseComplement(old_pattern):
-    dict = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
-    new_pattern = ''
-    for i in range(len(old_pattern)):
-        new_pattern = new_pattern + dict[old_pattern[-1-i]]
-    return new_pattern
-
-pattern = 'AAAACCCGGT'
-print(ReverseComplement(pattern))
